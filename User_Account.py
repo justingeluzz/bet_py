e2b60bcd03e306688ccae3fa635ef52ce81536fd
John Austin Geluz <geluz.johnaustin@gmail.com>
2023-10-18 17:03:21 +0800

    Third Commit: Updated backspaces

--- User_Account.py
+++ User_Account.py
@@ -17,23 +17,23 @@ class User_Account:
             return
         else:
             raise BalanceChecker(
-                f"Sorry. {self.name} only has the balance of {self.balance}"
+                f"\nSorry. {self.name} only has the balance of {self.balance}"
             )
     
     def withdraw_balance(self, amount):
         try:
             self.check_transaction(amount)
             self.balance = self.balance - amount
-            print("Withdraw complete")
+            print("\nWithdraw complete")
             self.check_balance()
 
         except BalanceChecker as error:
-            print(f"Withdraw failed: {error}")
+            print(f"\nWithdraw failed: {error}")
 
     
         
 account_name = input("Hello, What is your name? : ")
-account_balance = int(input(f"Hello, {account_name}. How much would you like to deposit? : "))
+account_balance = int(input(f"\nHello, {account_name}. How much would you like to deposit? : "))
     
 user_justin = User_Account(account_name, account_balance)
 
@@ -57,7 +57,7 @@ def Bet():
 
     
 def ask_again():
-    player_ask_again = input("Do you want to play again?\n 1. Yes \n 2. No \n ")
+    player_ask_again = input("\nDo you want to play again?\n 1. Yes \n 2. No \n ")
 
     if player_ask_again == "1":
 
@@ -78,15 +78,15 @@ def main():
             user_justin.check_balance()
 
         elif action_ask_user == "2":
-            player_ask_deposit_amount = int(input("How much do you want to deposit? : "))
+            player_ask_deposit_amount = int(input("\nHow much do you want to deposit? : "))
             user_justin.balance = user_justin.balance + player_ask_deposit_amount
-            print(f"Your balance is now: ${user_justin.balance}.")
+            print(f"\nYour balance is now: ${user_justin.balance}.")
         
         elif action_ask_user == "3":
             Bet()
 
         elif action_ask_user == "4":
-            player_ask_withdraw_amount = int(input("How much would you like to withdraw? : "))
+            player_ask_withdraw_amount = int(input("\nHow much would you like to withdraw? : "))
 
             user_justin.withdraw_balance(player_ask_withdraw_amount)
             
